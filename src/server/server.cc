e54e894e283ae23885c09e39d986345b44988244
lakes26 <lakes1126@gmail.com>
2023-04-30 17:34:39 -0400

    trying new stuff

--- src/server/server.cc
+++ src/server/server.cc
@@ -1,42 +1,60 @@
+
 #include <iostream>
 #include <memory>
 #include <string>
 
 #include <grpc++/grpc++.h>
+
+#ifdef BAZEL_BUILD
 #include "proto/kv.grpc.pb.h"
+#else
+#include "proto/kv.grpc.pb.h"
+#endif
 
-using namespace grpc;
+using grpc::Server;
+using grpc::ServerBuilder;
+using grpc::ServerContext;
+using grpc::Status;
+using kv::GetRequest;
+using kv::GetResponse;
+using kv::SetRequest;
+using kv::SetResponse;
+using kv::DeleteRequest;
+using kv::DeleteResponse;
+using kv::GetShardContentsRequest;
+using kv::GetShardContentsResponse;
+using kv::Kv;
 
-class KvServerImpl final : public kv::Kv::Service {
+class KvServerImpl final : public Kv::Service {
   
   Status Get(
     ServerContext* context,
-    const kv::GetRequest* request,
-    kv::GetResponse* response
+    const GetRequest* request,
+    GetResponse* response
   ) override {
     return Status::OK;
   }
  
   Status Set(
     ServerContext* context,
-    const kv::SetRequest* request,
-    kv::SetResponse* response
+    const SetRequest* request,
+    SetResponse* response
   ) override {
     return Status::OK;
   }
 
   Status Delete(
     ServerContext* context,
-    const kv::DeleteRequest* request,
-    kv::DeleteResponse* response
+    const DeleteRequest* request,
+    DeleteResponse* response
   ) override {
     return Status::OK;
   }
   
   Status GetShardContents(
     ServerContext* context,
-    const kv::GetShardContentsRequest* request,
-    kv::GetShardContentsResponse* response
+    const GetShardContentsRequest* request,
+    GetShardContentsResponse* response
   ) override {
     return Status::OK;
   }
@@ -48,16 +66,16 @@ void RunServer() {
   ServerBuilder builder;
 
   // Listen on the given address without any authentication mechanism.
-  builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
+  //builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
   // Register "service" as the instance through which we'll communicate with
   // clients. In this case it corresponds to an *synchronous* service.
-  builder.RegisterService(&service);
+  //builder.RegisterService(&service);
   // Finally assemble the server.
-  std::unique_ptr<Server> server(builder.BuildAndStart());
-  std::cout << "Server listening on " << server_address << std::endl;
+  //std::unique_ptr<Server> server(builder.BuildAndStart());
+  //std::cout << "Server listening on " << server_address << std::endl;
   // Wait for the server to shutdown. Note that some other thread must be
   // responsible for shutting down the server for this call to ever return.
-  server->Wait();
+  //server->Wait();
 }
 
 int main(int argc, char** argv) {
